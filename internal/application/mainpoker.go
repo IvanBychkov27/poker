58efdfd08da868a0b26b287f5837a7f622386bed
Ivan <IvanBychkov@mail.ru>
2021-09-01 19:53:13 +0300

    Добавлен вывод карты

--- internal/application/mainpoker.go
+++ internal/application/mainpoker.go
@@ -1,8 +1,11 @@
 package application
 
 import (
+	"encoding/base64"
+	"go.uber.org/zap"
+	"io/ioutil"
 	"net/http"
-	"time"
+	"strings"
 )
 
 const pagePoker string = `
@@ -10,14 +13,33 @@ const pagePoker string = `
 <html>
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="refresh" content="10">
 		<title>Poker</title>
 	</head>
 <body>
-	<p><b>The Poker project is working </b>
-	<p>
+	<p><b>Poker</b>
+	<p><img src="data:image/jpg; base64,{IMAGE}">
+</body>
+</html>
 `
 
 func (app *Application) mainpoker(rw http.ResponseWriter, _ *http.Request) {
-	rw.Write([]byte(pagePoker + time.Now().Format("02-01-2006 15:04:05") + "</body></html>"))
+	pic := app.getPicture()
+	if pic == nil {
+		return
+	}
+
+	dataBase64 := base64.StdEncoding.EncodeToString(pic)
+	data := strings.Replace(pagePoker, "{IMAGE}", dataBase64, -1)
+	rw.Write([]byte(data))
+}
+
+func (app *Application) getPicture() []byte {
+	openFileName := "cardImage/144.jpg"
+	fileData, err := ioutil.ReadFile(openFileName)
+	if err != nil {
+		app.logger.Error("error open picture", zap.Error(err))
+		return nil
+	}
+
+	return fileData
 }
